45d77bd64250e92b2854ef2b9cf8c633f3c00977
little bear <zyst8616@users.noreply.github.com>
2026-06-29 16:40:12 +0800

    Deploy GitHub Pages

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "two-person-todo-v1";
+const SCOPE_PATH = new URL(self.registration.scope).pathname;
+const APP_SHELL = [SCOPE_PATH, `${SCOPE_PATH}manifest.webmanifest`, `${SCOPE_PATH}icons/icon.svg`];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(SCOPE_PATH)))
+  );
+});
